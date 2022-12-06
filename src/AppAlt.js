12d9457e1cddb15775efd676627456fbd534fbeb
laREiGN <43932616+laREiGN@users.noreply.github.com>
2022-12-06 15:20:11 +0100

    Edited page coming soon, build with only placeholder page

---
+++ src/AppAlt.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { PageNotFound } from './components/pages';
+
+import './index.css';
+import './styling/Buttons.css'
+
+export default function AppAlt() {
+
+  return (
+    <div>
+      <PageNotFound />
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+// import AppMain from './AppMain';
+import AppAlt from './AppAlt';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <AppAlt />
   </BrowserRouter>
 );
