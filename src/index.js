5417c10a4aeaabf166a7a95b130f7cc4d22a2f9a
Girts Dvinskis <43932616+laREiGN@users.noreply.github.com>
2022-12-26 12:58:21 +0100

    Introduced new color variables

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import AppMain from './AppMain';
+import AppMain from './AppMain';
 import AppAlt from './AppAlt';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <AppAlt />
+    <AppMain />
   </BrowserRouter>
 );
